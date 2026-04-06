77f45a552645d436576f2ab95b0d99eb380734f7
cleonavila <cleon@almagrario.com>
2026-04-06 14:10:23 -0500

    Ajustes

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
-  imports: [],
-  templateUrl: './change-detection.component.html',
+  imports: [TitleComponent, CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+  <app-title [title]="currentFramework()"></app-title>
+  <pre>{{frameworkSignal() | json }}</pre>
+  <pre>{{frameworkProperty | json }}</pre>
+  `,
   styles: ``
 })
 export default class ChangeDetectionComponent {
 
+  public currentFramework = computed(() => `Change Detection ${this.frameworkSignal().name}`);
+
+  public frameworkSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016
+  });
+
+  public frameworkProperty = {
+    name: 'Angular',
+    releaseDate: 2016
+  };
+
+  constructor() {
+    setTimeout(() => {
+      //this.frameworkProperty.name = "React";
+      this.frameworkSignal.update(value => ({
+        ...value,
+        name: "React"
+      }));
+      console.log("Hecho");
+    }, 3000);
+  }
 }
--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { booleanAttribute, Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   imports: [],
-  templateUrl: './title.component.html',
+  template: `
+  <h1 class="text-bold text-3xl mb-4">{{title}}</h1>
+  `,
   styles: ``
 })
 export class TitleComponent {
+
+  //public title = input.required();
+
+  @Input({required: true}) title: string = "";
+
+  @Input({transform: booleanAttribute}) withShadow: boolean = false;
 
 }
